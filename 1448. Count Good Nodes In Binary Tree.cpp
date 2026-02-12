12759e19dc8a703ebd21ead2aaf53b75db5f86e1
Ayushi Baidya <ayushibaidya@gmail.com>
2026-02-12 16:48:35 -0500

    1448

---
+++ 1448. Count Good Nodes In Binary Tree.cpp
@@ -0,0 +1,30 @@
+/*
+we have to get a new count every iteration, count = 1 or 0 based on if there is a node
+greater than the maxNode val 
+
+therefore, we cannot pass the node count value as a parameter 
+
+TC: O(n)
+*/
+
+class Solution{
+    public: 
+    int countGoodNodes(TreeNode* root, int maxNode){
+        if(root == NULL) return 0; 
+
+        int count = 0; 
+
+        if(root->val >= maxNode){
+            count = 1; 
+        }
+
+        count += countGoodNodes(root->left, maxNode); 
+        count += countGoodNodes(root->right, maxNode); 
+
+        return count; 
+    }
+
+    int goodNodes(TreeNode* root){
+        return countGoodNodes(root, root->val); 
+    }
+}
