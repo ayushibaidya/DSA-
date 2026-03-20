4fc4ed43f0fc75726b7fbe77171bdba8d181adc3
Ayushi Baidya <ayushibaidya@gmail.com>
2026-03-20 16:42:53 -0400

    TWO SUM

--- Neetcode150/Arrays/1. Two Sum.cpp
+++ Neetcode150/Arrays/1. Two Sum.cpp
@@ -1,3 +1,24 @@
+//brute force - two nested loops 
+//TC: O(n) SC: O(1)
+vector<int> twoSum(vector<int>& nums, int target) {
+        int size = nums.size(); 
+
+        vector<int>ans; 
+
+        for(int i = 0; i < size; i++){
+            for(int j = i+1; j < size; j++){
+                if(nums[i] + nums[j] == target){
+                    ans.push_back(i); 
+                    ans.push_back(j); 
+                    return ans; 
+                }
+            }
+        }
+        return ans; 
+    }
+
+//unordered_map 
+//TC: O(n) SC: O(n)
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
@@ -18,3 +39,28 @@ public:
         return ans; 
     }
 };
+
+//if we have to optimize space we can sort the array and use two pointers but that will make it 
+//TC: O(nlogn) and SC: O(1)
+vector<int> twoSum(vector<int>& nums, int target) {
+        vector<pair<int, int>> ans; 
+        for(int i = 0; i < nums.size(); i++){
+            ans.push_back({nums[i], i}); 
+        }
+
+        sort(ans.begin(), ans.end()); 
+
+        int low = 0; int high = nums.size()-1; 
+
+        while(low < high){
+            int sum = ans[low].first + ans[high].first; 
+            if(sum == target){
+                return {ans[low].second, ans[high].second}; 
+            }else if(sum < target){
+                low++; 
+            }else{
+                high--; 
+            }
+        }
+        return {};
+    }
